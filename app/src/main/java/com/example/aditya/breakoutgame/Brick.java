e88641de72a33acd33f4349d2b5bfc84397453be
Aditya <mahajanaditya007@gmail.com>
2015-11-17 15:38:31 -0600

    Added sounds

---
+++ app/src/main/java/com/example/aditya/breakoutgame/Brick.java
@@ -0,0 +1,37 @@
+package com.example.aditya.breakoutgame;
+
+/**
+ * Created by aditya on 11/17/15.
+ */
+import android.graphics.RectF;
+
+public class Brick {
+
+    private RectF rect;
+
+    private boolean isVisible;
+
+    public Brick(int row, int column, int width, int height){
+
+        isVisible = true;
+
+        int padding = 1;
+
+        rect = new RectF(column * width + padding,
+                row * height + padding,
+                column * width + width - padding,
+                row * height + height - padding);
+    }
+
+    public RectF getRect(){
+        return this.rect;
+    }
+
+    public void setInvisible(){
+        isVisible = false;
+    }
+
+    public boolean getVisibility(){
+        return isVisible;
+    }
+}
